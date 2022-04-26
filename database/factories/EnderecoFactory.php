fe439ccae8678dc186385a392f7d90a79d057de5
Danilo Ryan Oliveira <47749142+DanRyan11@users.noreply.github.com>
2022-04-26 19:40:37 -0300

    Criando factory endereco e cidade

---
+++ database/factories/EnderecoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Factory>
+ */
+class EnderecoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'logradouro' => $this->faker->streetName,
+            'numero'     => $this->faker->numberBetween(1, 1000),
+            'bairro'     => $this->faker->streetName,
+            'cidade_id'  => $this->faker->numberBetween(1, 10),
+        ];
+    }
+}
--- database/migrations/2022_04_26_210356_create_enderecos_table.php
+++ database/migrations/2022_04_26_210356_create_enderecos_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('complemento')->nullable();
             $table->unsignedBigInteger('cidade_id');
             $table->timestamps();
-            $table->binary('status');
+            $table->binary('status')->default(1);
         });
     }
 

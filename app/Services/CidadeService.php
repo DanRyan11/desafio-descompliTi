a5b0b169875f2afe1d00777b6c991d7f9abcf41d
Danilo Ryan Oliveira <47749142+DanRyan11@users.noreply.github.com>
2022-04-26 18:43:52 -0300

    Cidade e Endereco Service

--- app/Services/CidadeService.php
+++ app/Services/CidadeService.php
@@ -2,6 +2,23 @@
 
 namespace App\Services;
 
+use App\Repositories\CidadeRepository;
+
 class CidadeService{
-    
+    protected $repository;
+
+    public function __construct(CidadeRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function get($id = false)
+    {
+        return $id ? $this->repository->get($id) : $this->repository->getAll();
+    }
+
+    public function create(array $data)
+    {
+        return $this->repository->create($data);
+    }
 }
--- app/Services/EnderecoService.php
+++ app/Services/EnderecoService.php
@@ -2,6 +2,33 @@
 
 namespace App\Services;
 
+use App\Repositories\EnderecoRepository;
+
 class EnderecoService{
-    
+    protected $repository;
+
+    public function __construct(EnderecoRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function get($id = false)
+    {
+        return $id ? $this->repository->get($id) : $this->repository->getAll();
+    }
+
+    public function create(array $data)
+    {
+        return $this->repository->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        return $this->repository->update($id,$data);
+    }
+
+    function delete($id)
+    {
+        return $this->repository->delete($id);
+    }
 }

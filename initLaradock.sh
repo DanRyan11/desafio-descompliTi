073833b9346544e92192b66dba91919c239b98cc
Danilo Ryan Oliveira <47749142+DanRyan11@users.noreply.github.com>
2022-04-28 13:30:41 -0300

    Correção de diretório para clone do laradock

--- initLaradock.sh
+++ initLaradock.sh
@@ -2,19 +2,19 @@
 
 echo "--------Configurando o ambiente do laradock...--------"
 
-if [ ! -d ../laradock ]
+if [ ! -d ./laradock ]
 then
     echo "Clonando repositório laradock"
-    git clone https://github.com/Laradock/laradock.git ../laradock;
+    git clone https://github.com/Laradock/laradock.git ./laradock;
     echo "Sucesso: laradock clonado"
 fi
 
 echo "Configurando .env"
-cp .env.laradock ../laradock/.env;
+cp .env.laradock ./laradock/.env;
 echo "Sucesso: .env configurado!"
 
 echo "Subindo os containers do Nginx e do Mysql"
-cd ../laradock;docker-compose up -d nginx mysql;
+cd ./laradock;docker-compose up -d nginx mysql;
 echo "Sucesso: containers online"
 
 echo "--------Ambiente configurado com sucesso--------"
